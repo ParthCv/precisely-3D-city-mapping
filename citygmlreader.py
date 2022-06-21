fc51fa5390aa414b5d1e3e967f2b0766670b439c
Parth Chaturvedi <parthcv0101@gmail.com>
2022-06-21 09:07:01 +0530

    Build cityGML reader code and doc strings

---
+++ citygmlreader.py
@@ -0,0 +1,122 @@
+from vtk import vtkCityGMLReader
+from vtk import vtkJPEGReader
+from vtk import vtkTexture
+from vtkmodules.vtkRenderingCore import (
+    vtkActor,
+    vtkRenderWindow,
+    vtkRenderWindowInteractor,
+    vtkRenderer,
+    vtkPolyDataMapper
+)
+
+
+class CityGMLReader:
+
+    @staticmethod
+    def simulate(filePath):
+        """
+        Reads the .gml file and processes it in a 3D Model window
+        :param filePath: Path to the .gml file
+        :return: None
+        """
+
+        # The renderer renders into the render window. And set the background.
+        ren = vtkRenderer()
+        ren.SetBackground(0.5, 0.7, 0.7)
+
+        # The render window interactor captures mouse events and will
+        # perform appropriate camera or actor manipulation depending on the
+        # nature of the events.
+        renWin = vtkRenderWindow()
+        renWin.AddRenderer(ren)
+
+        iren = vtkRenderWindowInteractor()
+        iren.SetRenderWindow(renWin)
+
+        # Initialize the CityGML reader and set the file path
+        reader = vtkCityGMLReader()
+        reader.SetFileName(filePath)
+
+        # Specify the level of detail to read (0-4) [default - 3]
+        reader.SetLOD(2)
+
+        # Brings the reader up-to-date
+        reader.Update()
+
+        # multi block dataset - organizes a dataset into blocks
+        mb = reader.GetOutput()
+
+        # Set an iterator to read over all the blocks in the dataset.
+        # And an object to always store current data object
+        it = mb.NewIterator()
+        obj = it.GetCurrentDataObject()
+
+        while obj:
+            # This is a PolyData object which represents a geometric structure
+            # consisting of vertices, lines, polygons, and/or triangle strips
+            poly = it.GetCurrentDataObject()
+
+            if poly:
+
+                # This mapper helps map the polygonal data to graphics primitives,
+                # that are the basic objects for the creation of complex images.
+                mapper = vtkPolyDataMapper()
+                mapper.SetInputDataObject(poly)
+
+                # Used to represent an object in a rendered scene. Set the mapper
+                # to tha actor and add it to the renderer
+                actor = vtkActor()
+                actor.SetMapper(mapper)
+                ren.AddActor(actor)
+
+                # Retrieve the general field data and get the array with the name texture-uri,
+                # which contains the path to the texture file.
+                textureField = poly.GetFieldData().GetAbstractArray('texture_uri')
+                if textureField:
+                    textureURI = textureField.GetValue(0)
+
+                    # complete path to the texture file and read it as a jpeg
+                    path = filePath + '/' + textureURI
+                    JpegReader = vtkJPEGReader()
+                    JpegReader.SetFileName(path)
+
+                    # We create a texture object which handles the loading and binding if texture maps
+                    # We set the connection for the given input port index, this also removes  all other
+                    # connections from the port. To add the port we use GetOutputPort on the jpeg reader
+                    texture = vtkTexture()
+                    texture.SetInputConnection(JpegReader.GetOutputPort())
+
+                    # turns the linear interpolation on. which means to estimate an unknown value from given
+                    # data assuming the curve is a straight line.
+                    texture.InterpolateOn()
+
+                    # set the texture of the current actor.
+                    actor.SetTexture(texture)
+
+            # iterate to the next block
+            it.GoToNextItem()
+            obj = it.GetCurrentDataObject()
+
+        # Automatically sets up the camera based on the bound
+        ren.ResetCamera()
+
+        # We get the current camera with GetActiveCamera
+        # Azimuth - horizontal rotation of the camera
+        # Roll - Spin the camera around its axis
+        # Zoom - zooms in (>1) or out (<1) based on the factor
+        ren.GetActiveCamera().Azimuth(90)
+        ren.GetActiveCamera().Roll(90)
+        ren.GetActiveCamera().Zoom(1.5)
+
+        # Set the size of the rendering window in pixels
+        renWin.SetSize(400, 400)
+
+        # Ask each viewport in the render window to render its image
+        renWin.Render()
+
+        # Prepare for handling events and set the Enabled flag to true.
+        iren.Initialize()
+        renWin.Render()
+
+        # Start the event loop
+        iren.Start()
--- main.py
+++ main.py
@@ -1,94 +1,40 @@
-#!/usr/bin/env python
+import argparse
+import os.path
+from pathlib import Path
 
-# This simple example shows how to do basic rendering and pipeline
-# creation.
+from citygmlreader import CityGMLReader
 
-# noinspection PyUnresolvedReferences
-import vtkmodules.vtkInteractionStyle
-# noinspection PyUnresolvedReferences
-import vtkmodules.vtkRenderingOpenGL2
-from vtkmodules.vtkCommonColor import vtkNamedColors
-from vtkmodules.vtkFiltersSources import vtkCylinderSource
-from vtkmodules.vtkRenderingCore import (
-    vtkActor,
-    vtkPolyDataMapper,
-    vtkRenderWindow,
-    vtkRenderWindowInteractor,
-    vtkRenderer
-)
 
-def camera_modified_callback(caller, event):
+def setup_commandline_request():
     """
-     Used to estimate positions similar to the book illustrations.
-    :param caller:
-    :param event:
-    :return:
+    Initializes commandline arguments, checks the inputs and calls the simulate function.
+    :return:None
     """
-    print(caller.GetClassName(), "modified")
-    # Print the interesting stuff.
-    res = f'\tcamera = renderer.GetActiveCamera()\n'
-    res += f'\tcamera.SetPosition({", ".join(map("{0:0.6f}".format, caller.GetPosition()))})\n'
-    res += f'\tcamera.SetFocalPoint({", ".join(map("{0:0.6f}".format, caller.GetFocalPoint()))})\n'
-    res += f'\tcamera.SetViewUp({", ".join(map("{0:0.6f}".format, caller.GetViewUp()))})\n'
-    res += f'\tcamera.SetDistance({"{0:0.6f}".format(caller.GetDistance())})\n'
-    res += f'\tcamera.SetClippingRange({", ".join(map("{0:0.6f}".format, caller.GetClippingRange()))})\n'
-    print(res)
+    parser = argparse.ArgumentParser(description='Process a 3D Model.')
+    parser.add_argument("filepath", help='an 3D model file')
+
+    args = parser.parse_args()
+    filePath = Path(args.filepath)
+    if filePath.is_file():
+        split_tup = os.path.splitext(filePath)
+        # extract the file extension
+        file_extension = split_tup[1]
+        validFileTypes = ['.gml', '.xml']
+        if file_extension == '.gml':
+            try:
+                CityGMLReader.simulate(filePath)
+            except Exception as e:
+                print(f'Error: Something went wrong when reading the file.')
+                print(e)
+        else:
+            print(f'Error: Invalid file extension!')
+            print(f'Allowed file types {validFileTypes}')
+    else:
+        print(f'Error: file {filePath} does not exist!')
+
 
 def main():
-    colors = vtkNamedColors()
-    # Set the background color.
-    bkg = map(lambda x: x / 255.0, [26, 51, 102, 255])
-    colors.SetColor("BkgColor", *bkg)
-
-    # This creates a polygonal cylinder model with eight circumferential
-    # facets.
-    cylinder = vtkCylinderSource()
-    cylinder.SetResolution(8)
-
-    # The mapper is responsible for pushing the geometry into the graphics
-    # library. It may also do color mapping, if scalars or other
-    # attributes are defined.
-    cylinderMapper = vtkPolyDataMapper()
-    cylinderMapper.SetInputConnection(cylinder.GetOutputPort())
-
-    # The actor is a grouping mechanism: besides the geometry (mapper), it
-    # also has a property, transformation matrix, and/or texture map.
-    # Here we set its color and rotate it -22.5 degrees.
-    cylinderActor = vtkActor()
-    cylinderActor.SetMapper(cylinderMapper)
-    cylinderActor.GetProperty().SetColor(colors.GetColor3d("Tomato"))
-    cylinderActor.RotateX(30.0)
-    cylinderActor.RotateY(-45.0)
-
-    # Create the graphics structure. The renderer renders into the render
-    # window. The render window interactor captures mouse events and will
-    # perform appropriate camera or actor manipulation depending on the
-    # nature of the events.
-    ren = vtkRenderer()
-    renWin = vtkRenderWindow()
-    renWin.AddRenderer(ren)
-    iren = vtkRenderWindowInteractor()
-    iren.SetRenderWindow(renWin)
-
-    # Add the actors to the renderer, set the background and size
-    ren.AddActor(cylinderActor)
-    ren.SetBackground(colors.GetColor3d("BkgColor"))
-    renWin.SetSize(300, 300)
-    renWin.SetWindowName('CylinderExample')
-
-    # This allows the interactor to initalize itself. It has to be
-    # called before an event loop.
-    iren.Initialize()
-
-    # We'll zoom in a little by accessing the camera and invoking a "Zoom"
-    # method on it.
-    ren.ResetCamera()
-    ren.GetActiveCamera().Zoom(1.5)
-    renWin.Render()
-    ren.GetActiveCamera().AddObserver('ModifiedEvent', camera_modified_callback)
-
-    # Start the event loop.
-    iren.Start()
+    setup_commandline_request()
 
 
 if __name__ == '__main__':
