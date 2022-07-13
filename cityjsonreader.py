2d9e4ad72cc4c7c5ae6b24ee38b44789e3909257
Parth Chaturvedi <parthcv0101@gmail.com>
2022-07-13 21:20:44 +0530

    Add dostrings

--- cityjsonreader.py
+++ cityjsonreader.py
@@ -9,12 +9,22 @@ class CityJSONReader:
 
     @staticmethod
     def simulate(filePath, lod):
+        """
+        Validates and then converts a CityJSON file to a gml file and then renders the new gml file
+        :param filePath: Path to a json file
+        :param lod: level of detail
+        :return: None
+        """
+
+        # Path to the city gml tool executable file to convert the file
         city_gml_tool_path = 'citygml-tools-1.4.5\\citygml-tools.bat'
 
+        # Validate the CityJSON file before processing
         validate_file = subprocess.run(['cjio', filePath, 'validate'], capture_output=True, text=True)
         validate_file.check_returncode()
 
         try:
+            # Run the command to convert the file
             subprocess.run(
                 [city_gml_tool_path, 'from-cityjson', os.path.abspath(str(filePath))],
                 capture_output=True,
@@ -25,8 +35,11 @@ class CityJSONReader:
             split_tup = os.path.splitext(filePath)
             # extract the file name
             file_name = split_tup[0]
+
+            # The file path to new gml file
             new_file = Path(file_name + '.gml')
 
+            # Check the file and render the gml file
             if new_file.is_file():
                 if lod is None:
                     CityGMLReader.simulateWithoutLOD(new_file)
